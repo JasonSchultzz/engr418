5a6c6379305637d91e59023ae85e7f82e3aef60e
Jason Schultz <jason.schultz@hotmail.ca>
2024-10-28 22:19:29 -0700

    Fixed eigenvector function

--- assignments/assignment_5.py
+++ assignments/assignment_5.py
@@ -1,13 +1,14 @@
 import numpy as np
 
+P = 5
 X = np.array([
-    [-0.4]
+    [-0.4, -0.4, 0.1, 0.1, 0.6],
+    [0.4, -0.1, -0.6, 0.4, -0.1]
 ])
+M = (1/P)*np.matmul(X, X.T)
+print(M)
 
-M = np.array([
-    [0.7, -0.2],
-    [-0.2, 0.7]
-])
 D, V = np.linalg.eig(M)
 
-print(D, V)
+print(D)
+print(V)
